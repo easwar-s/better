3ec87a9a6b2fdacafdf4bc5d8dd0253e1435414a
easwar-s <49307475+easwar-s@users.noreply.github.com>
2019-04-20 12:00:12 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter the character\n");
     scanf("%c",&v);
     if(v=='a'||v=='A'||v=='e'||v=='E'||v=='i'||v=='I'||v=='o'||v=='O'||v=='u'||v=='U')
-        print("%c is vowel.\n",v);
+        printf("%c is vowel.\n",v);
     else
        printf("%c is a consonant.",v);
     return 0;
