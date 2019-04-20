91ab1cf19e37633a9d9aca4fc7754ea031be86a4
easwar-s <49307475+easwar-s@users.noreply.github.com>
2019-04-20 11:43:08 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+v = input("input a letter of the alphabet:")
+if v in('a','e','i','o','u'):
+    print(%s is a vowel."%v)
+elif v=='y':
+    print("sometimes letter y stands for vowel and not.")
+else:
+    print("%s is a consonant."%v)
