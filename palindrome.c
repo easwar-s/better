91b646a9e7ad345a708512f68b9bf6c81596c3a0
easwar-s <49307475+easwar-s@users.noreply.github.com>
2019-04-22 09:34:50 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int maoin()
+{
+ int n,reverse = 0,t;
+ printf("Enter the number to check if it is a palindrome or not \n");
+ scanf("%d",&n);
+ t = n;
+ while(t != 0)
+ {
+  reverse = reverse *10;
+  reverse = reverse + t%10;
+  t = t/10;
+ }
+ if(n == reverse)
+  printf("%d is a palindrome number.\n",n);
+ else
+  printf("%d isn't a palindrome number.\n",n);
+ return 0;
+} 
