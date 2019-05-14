f116010890fe5831dc9950ac0909d45f19c72f40
easwar-s <49307475+easwar-s@users.noreply.github.com>
2019-05-14 12:35:44 +0530

    prg47.py

---
+++ prg47.py
@@ -0,0 +1,7 @@
+ls=[]
+x=int(input("Enter how many number:"))
+for i in range(x):
+      num=int(input("Enter the number:))
+      ls.append(num)
+print("the max num is:",max(ls))
+print("the max num is:",min(ls))
